53109f838e0f07cc4e7f573bf97cc12da564bf81
edward-martyr <edwardmartyr@outlook.com>
2022-03-14 13:22:09 -0400

    format lua

--- kanbun.lua
+++ kanbun.lua
@@ -1,5 +1,5 @@
 -- Copyright 2022 Yuanhao Chen
-
+--
 -- This work may be distributed and/or modified under the
 -- conditions of the LaTeX Project Public License, either version 1.3
 -- of this license or (at your option) any later version.
@@ -14,22 +14,16 @@
 --
 -- This work consists of the files kanbun.sty, kanbun.lua,
 -- kanbun-luatex.sty, kanbun-example.tex and kanbun.tex.
-
-function directtex(str)
-    coroutine.yield(str)
-end
+function directtex(str) coroutine.yield(str) end
 
 function to_TeX_box(str)
-    directtex("\\ExplSyntaxOn\\newbox\\kanbun_lua_box\\sbox\\kanbun_lua_box{"..str.."}\\ExplSyntaxOff")
+    directtex("\\ExplSyntaxOn\\newbox\\kanbun_lua_box\\sbox\\kanbun_lua_box{" ..
+                  str .. "}\\ExplSyntaxOff")
     return tex.getbox('kanbun_lua_box')
 end
 
-function has_value (tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return true
-        end
-    end
+function has_value(tab, val)
+    for index, value in ipairs(tab) do if value == val then return true end end
     return false
 end
 
@@ -37,19 +31,19 @@ function main_loop()
     local brackets = {}
     -- 
     brackets["furigana"] = {"(", ")"}
-    brackets["okurigana"] = {"{","}"} -- allow user to omit
-    brackets["kaeriten"] = {"[","]"}
-    brackets["furigana4saidokumoji"] = {"‹","›"}
-    brackets["okurigana4saidokumoji"] = {"«","»"}
+    brackets["okurigana"] = {"{", "}"} -- allow user to omit
+    brackets["kaeriten"] = {"[", "]"}
+    brackets["furigana4saidokumoji"] = {"‹", "›"}
+    brackets["okurigana4saidokumoji"] = {"«", "»"}
     -- 
-    brackets["punctuation"] = {"⦉","⦊"}
+    brackets["punctuation"] = {"⦉", "⦊"}
     -- brackets["kanji"] = {"⌊","⌋"}
     -- brackets["multikanji"] = {"‘","’"}
     -- brackets["unit"] = {"“","”"}
     -- 
     local left_brackets = {}
     local right_brackets = {}
-    for k,v in pairs(brackets) do
+    for k, v in pairs(brackets) do
         table.insert(left_brackets, v[1])
         table.insert(right_brackets, v[2])
     end
@@ -61,33 +55,32 @@ function main_loop()
     local lines_chars_table = {}
     local tex_kana_bool = token.create("g_kana_bool")
     local tex_true_bool = token.create("c_true_bool")
-    for i,l in ipairs(verb_table) do
-        split_line = {}
+    for i, l in ipairs(verb_table) do
+        local split_line = {}
         for c in l:gmatch(utf8.charpattern) do
             table.insert(split_line, c)
         end
 
         -- some unknown bug (in \matchkana ?) solved by changing the last entry to an empty string if not ascii
-        for i,c in ipairs(split_line) do
-            last_index = i
-        end
-        if utf8.codepoint(split_line[last_index] or " ") < 128 then 
-            split_line[last_index+1]=""
+        for i, c in ipairs(split_line) do last_index = i end
+        if utf8.codepoint(split_line[last_index] or " ") < 128 then
+            split_line[last_index + 1] = ""
         else
-            split_line[last_index]=""
+            split_line[last_index] = ""
         end
 
-        last_bracket_index = 0
-        for j,c in ipairs(split_line) do
+        local last_bracket_index = 0
+        for j, c in ipairs(split_line) do
             if has_value(left_brackets, c) then
                 last_bracket_index = j
             end
             if last_bracket_index == 0 then
-                directtex("\\matchkana{"..c.."}")
-                if tex_kana_bool.mode == tex_true_bool.mode then 
-                    split_line[j] = brackets["okurigana"][1]..c..brackets["okurigana"][2]
+                directtex("\\matchkana{" .. c .. "}")
+                if tex_kana_bool.mode == tex_true_bool.mode then
+                    split_line[j] = brackets["okurigana"][1] .. c ..
+                                        brackets["okurigana"][2]
                 else
-                    split_line[j] = "“⌊"..c.."⌋”"
+                    split_line[j] = "“⌊" .. c .. "⌋”"
                 end
             end
             if has_value(right_brackets, c) then
@@ -98,24 +91,21 @@ function main_loop()
     end
 
     local annotated_lines_table = {}
-    for i,l in ipairs(lines_chars_table) do
-        line = table.concat(l, "")
+    for i, l in ipairs(lines_chars_table) do
+        local line = table.concat(l, "")
 
-        line = string.gsub(line, brackets["okurigana"][2]..brackets["okurigana"][1], "")
-        for k,v in pairs(brackets) do
-            line = string.gsub(line, "”(%"..v[1]..")", "%1")
-            line = string.gsub(line, "(%"..v[2]..")(“)", "%1”%2")
+        line = string.gsub(line, brackets["okurigana"][2] ..
+                               brackets["okurigana"][1], "")
+        for k, v in pairs(brackets) do
+            line = string.gsub(line, "”(%" .. v[1] .. ")", "%1")
+            line = string.gsub(line, "(%" .. v[2] .. ")(“)", "%1”%2")
         end
-        for c in line:gmatch(utf8.charpattern) do
-            last = c
-        end
-        if has_value(right_brackets, last) then 
-            str = str .. '”'
-        end
+        for c in line:gmatch(utf8.charpattern) do last = c end
+        if has_value(right_brackets, last) then str = str .. '”' end
         line = string.gsub(line, "⌊‘⌋”", "‘")
         line = string.gsub(line, "“⌊’⌋", "’")
         for p in punctuation_str:gmatch(utf8.charpattern) do
-            line = string.gsub(line, "”“⌊("..p..")⌋", "⦉%1⦊")
+            line = string.gsub(line, "”“⌊(" .. p .. ")⌋", "⦉%1⦊")
         end
         line = string.gsub(line, "⦊⦉", "")
 
@@ -123,16 +113,18 @@ function main_loop()
         line = string.gsub(line, "“⌊⌋”", "")
 
         -- process annotated text
-        tmp_number_of_multikanji_braces = {utf8.char(61442), utf8.char(61443)}
+        local tmp_number_of_multikanji_braces = {
+            utf8.char(61442), utf8.char(61443)
+        }
         -- process line into units
-        split_line = {}
+        local split_line = {}
         for c in line:gmatch(utf8.charpattern) do
             table.insert(split_line, c)
         end
-        units = {}
-        unit_content = {}
-        last_bracket_index = 0
-        for j,c in ipairs(split_line) do
+        local units = {}
+        local unit_content = {}
+        local last_bracket_index = 0
+        for j, c in ipairs(split_line) do
             if c == "”" then
                 last_bracket_index = last_bracket_index - 1
             end
@@ -147,61 +139,66 @@ function main_loop()
             end
         end
         -- account for multikanji
-        for j,u in ipairs(units) do
-            split_unit = {}
+        for j, u in ipairs(units) do
+            local split_unit = {}
             for c in u:gmatch(utf8.charpattern) do
                 table.insert(split_unit, c)
             end
-            number_of_multikanji = 0
+            local number_of_multikanji = 0
             local last_k
-            for k,c in ipairs(split_unit) do
+            for k, c in ipairs(split_unit) do
                 if c == "“" then
                     number_of_multikanji = number_of_multikanji + 1
                     last_k = k
                 end
             end
-            for m,c in ipairs(split_unit) do
+            for m, c in ipairs(split_unit) do
                 if m == last_k then
-                    split_unit[m] = "“"..tmp_number_of_multikanji_braces[1]..number_of_multikanji..tmp_number_of_multikanji_braces[2]
+                    split_unit[m] =
+                        "“" .. tmp_number_of_multikanji_braces[1] ..
+                            number_of_multikanji ..
+                            tmp_number_of_multikanji_braces[2]
                 end
             end
             units[j] = table.concat(split_unit, "")
             if number_of_multikanji > 0 then
-                units[j] = string.gsub(units[j], "“(.-)”’(.*)", "“%1%2”")
+                units[j] = string.gsub(units[j], "“(.-)”’(.*)",
+                                       "“%1%2”")
                 units[j] = string.gsub(units[j], "‘", "")
-                tmp_unit = units[j]
+                local tmp_unit = units[j]
                 table.remove(units, j)
-                for new_unit in tmp_unit:gmatch"“(.-)”" do
+                for new_unit in tmp_unit:gmatch "“(.-)”" do
                     table.insert(units, j, new_unit)
                     j = j + 1
                 end
             end
         end
-        for j,u in ipairs(units) do
-            if u == "" then
-                table.remove(units, j)
-            end
+        for j, u in ipairs(units) do
+            if u == "" then table.remove(units, j) end
         end
-        next_left_punct_ = ""
-        for j,u in ipairs(units) do
-            right_okuri_ = u:match("%{(.-)%}") or ""
-            kanji_ = u:match("⌊(.-)⌋") or ""
-            right_furi_ = u:match("%((.-)%)") or ""
-            left_furi_ = u:match("‹(.-)›") or ""
-            left_okuri_ = u:match("«(.-)»") or ""
-            punct_ = u:match("⦉(.-)⦊") or ""
+        local next_left_punct_ = ""
+        for j, u in ipairs(units) do
+            local right_okuri_ = u:match("%{(.-)%}") or ""
+            local kanji_ = u:match("⌊(.-)⌋") or ""
+            local right_furi_ = u:match("%((.-)%)") or ""
+            local left_furi_ = u:match("‹(.-)›") or ""
+            local left_okuri_ = u:match("«(.-)»") or ""
+            local punct_ = u:match("⦉(.-)⦊") or ""
             --
             -- punct_ = punct_:gsub("―", "\\tateten")
             -- punct_ = punct_:gsub("—", "\\tateten")
             -- punct_ = punct_:gsub("〻", "\\ninojiten")
             --
-            kaeriten_ = u:match("%[(.-)%]") or ""
-            multikanji_ = u:match(tmp_number_of_multikanji_braces[1].."(.-)"..tmp_number_of_multikanji_braces[2]) or 0
+            local kaeriten_ = u:match("%[(.-)%]") or ""
+            local multikanji_ = u:match(tmp_number_of_multikanji_braces[1] ..
+                                            "(.-)" ..
+                                            tmp_number_of_multikanji_braces[2]) or
+                                    0
             if punctuation_str:match(kanji_) then
                 punct_ = kanji_
                 kanji_ = ""
             end
-            left_punct_ = next_left_punct_
+            local left_punct_ = next_left_punct_
             next_left_punct_ = ""
             for p_ in punct_:gmatch(utf8.charpattern) do
                 if left_punctuation_str:match(p_) then
@@ -212,17 +209,33 @@ function main_loop()
                 punct_ = string.gsub(punct_, p_, "")
             end
             if u:match(tmp_number_of_multikanji_braces[1]) then
-                multiruby_raise_by_ = 0
-                for trace_back_index = 1, multikanji_-1 do
-                    multiruby_raise_by_ = multiruby_raise_by_ + to_TeX_box(units[j - trace_back_index]).width/(kanbunzwtosp)
+                local multiruby_raise_by_ = 0
+                for trace_back_index = 1, multikanji_ - 1 do
+                    multiruby_raise_by_ =
+                        multiruby_raise_by_ +
+                            to_TeX_box(units[j - trace_back_index]).width /
+                            (kanbunzwtosp)
                 end
-                units[j -  multikanji_ + 1] = units[j -  multikanji_ + 1]:gsub("\\kanjiunit", "\\hbox{\\kanjiunit")
-                units[j] = "\\kanjiunit{\\multifuriokuri["..multiruby_raise_by_.."]{"..right_furi_.."}{"..right_okuri_.."}}{"..left_punct_.."}{"..kanji_.."}{"..punct_.."}{"..kaeriten_.."}{\\multifuriokuri["..multiruby_raise_by_.."]{"..left_furi_.."}{"..left_okuri_.."}}}"
+                units[j - multikanji_ + 1] =
+                    units[j - multikanji_ + 1]:gsub("\\kanjiunit",
+                                                    "\\hbox{\\kanjiunit")
+                units[j] = "\\kanjiunit{\\multifuriokuri[" ..
+                               multiruby_raise_by_ .. "]{" .. right_furi_ ..
+                               "}{" .. right_okuri_ .. "}}{" .. left_punct_ ..
+                               "}{" .. kanji_ .. "}{" .. punct_ .. "}{" ..
+                               kaeriten_ .. "}{\\multifuriokuri[" ..
+                               multiruby_raise_by_ .. "]{" .. left_furi_ .. "}{" ..
+                               left_okuri_ .. "}}}"
             else
                 if kanji_ == "" then
                     units[j] = ""
                 else
-                    units[j] = "\\kanjiunit{\\furiokuri{"..right_furi_.."}{"..right_okuri_.."}}{"..left_punct_.."}{"..kanji_.."}{"..punct_.."}{"..kaeriten_.."}{\\furiokuri{"..left_furi_.."}{"..left_okuri_.."}}"
+                    units[j] =
+                        "\\kanjiunit{\\furiokuri{" .. right_furi_ .. "}{" ..
+                            right_okuri_ .. "}}{" .. left_punct_ .. "}{" ..
+                            kanji_ .. "}{" .. punct_ .. "}{" .. kaeriten_ ..
+                            "}{\\furiokuri{" .. left_furi_ .. "}{" ..
+                            left_okuri_ .. "}}"
                 end
             end
         end
@@ -232,13 +245,15 @@ function main_loop()
     end
 
     -- ouput
-    output = "{\\kanbunfont"..table.concat(annotated_lines_table, "\\par").."\\par}"
-    directtex("\\def\\printkanbun{"..output.."}")
+    output = "{\\kanbunfont" .. table.concat(annotated_lines_table, "\\par") ..
+                 "\\par}"
+    directtex("\\def\\printkanbun{" .. output .. "}")
     directtex("\\def\\printkanbuncode{\\directlua{print('')print(output)}}")
     -- nopar output
     nopar_output = table.concat(annotated_lines_table, "\\\\")
-    directtex("\\def\\printkanbunnopar{"..nopar_output.."}")
-    directtex("\\def\\printkanbunnoparcode{\\directlua{print('')print(nopar_output)}}")
+    directtex("\\def\\printkanbunnopar{" .. nopar_output .. "}")
+    directtex(
+        "\\def\\printkanbunnoparcode{\\directlua{print('')print(nopar_output)}}")
 
     -- end loop in TeX
     directtex("\\continuefalse")
@@ -248,17 +263,20 @@ end
 function determine_ichire_kern()
     local prev_kern = 0
     local curr_kern = -10
-    
-    local ichirekern = tostring(approximate(curr_kern, prev_kern, ichire_untouched)/100)
 
-    directtex("\\setkanbun{ichirekern="..ichirekern.."}\\kanbun@msg{Automatically setting ichirekern to "..ichirekern..".}")
+    local ichirekern = tostring(approximate(curr_kern, prev_kern,
+                                            ichire_untouched) / 100)
+
+    directtex("\\setkanbun{ichirekern=" .. ichirekern ..
+                  "}\\kanbun@msg{Automatically setting ichirekern to " ..
+                  ichirekern .. ".}")
 
     -- end loop in TeX
     directtex("\\continuefalse")
 end
 
 function ichire_node_len(kerning)
-    directtex("\\kanbun@setichireboxkerning["..kerning.."]")
+    directtex("\\kanbun@setichireboxkerning[" .. kerning .. "]")
     return node.length(tex.box["kanbun@ichirebox"].head)
 end
 
@@ -268,12 +286,12 @@ function ichire_untouched(kern)
 end
 
 function approximate(curr, prev, func)
-    while math.abs(prev-curr)>0.1 or func(prev) == func(curr) do
+    while math.abs(prev - curr) > 0.1 or func(prev) == func(curr) do
         if func(prev) == func(curr) then
             prev, curr = curr, curr + curr - prev
         else
-            prev, curr = curr, (curr + prev)/2
+            prev, curr = curr, (curr + prev) / 2
         end
     end
-    return math.max(curr, prev)
+    return math.max(curr, prev) - 1
 end
--- outlinejfontpaths.lua
+++ outlinejfontpaths.lua
@@ -1,5 +1,5 @@
 -- adapted from https://tex.stackexchange.com/a/451951
-
+--
 -- We need some utilities from ConTeXt
 callbacks = callbacks or {}
 callbacks.supported = callbacks.supported or {}
@@ -14,28 +14,22 @@ function outlinejfontpaths(character)
     -- prioritise using tate luatexja font
     local curjfnt = tex.getattribute('ltj@curjfnt')
     local curtfnt = tex.getattribute('ltj@curtfnt')
-    if curjfnt >= 0 then
-        fontid = curjfnt
-    end
-    if curtfnt >= 0 then
-        fontid = curtfnt
-    end
+    if curjfnt >= 0 then fontid = curjfnt end
+    if curtfnt >= 0 then fontid = curtfnt end
 
-    local shapedata   = fonts.hashes.shapes[fontid] -- by index
-    local chardata    = fonts.hashes.characters[fontid] -- by unicode
-    local shapeglyphs = shapedata.glyphs or { }
+    local shapedata = fonts.hashes.shapes[fontid] -- by index
+    local chardata = fonts.hashes.characters[fontid] -- by unicode
+    local shapeglyphs = shapedata.glyphs or {}
 
     character = utf.byte(character)
     local c = chardata[character]
     if c then
-        if not c.index then
-            return {}
-        end
+        if not c.index then return {} end
         local glyph = shapeglyphs[c.index]
         if glyph and (glyph.segments or glyph.sequence) then
-            local units  = shapedata.units or 1000
-            local factor = 100/units
-            local paths  = fonts.metapost.paths(glyph,factor)
+            local units = shapedata.units or 1000
+            local factor = 100 / units
+            local paths = fonts.metapost.paths(glyph, factor)
             return paths
         end
     end
